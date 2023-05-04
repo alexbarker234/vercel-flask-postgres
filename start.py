87dd5cc76d4357e6cc20dcfd9d37c6d4fafa4c59
alexbarker234 <alex.barker234@gmail.com>
2023-05-04 17:39:04 +0800

    postgresql

--- start.py
+++ start.py
@@ -1,11 +1,29 @@
-from flask import Flask
+from datetime import datetime
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+db = SQLAlchemy()
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv('POSTGRES_URL').replace("postgres://", "postgresql://") or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+db.init_app(app)
+
+class PageGets(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.DateTime, nullable=False)
 
 @app.route('/')
-def home():
-    return 'Hello, World!'
+def home():   
+    listen = PageGets(date = datetime.utcnow())
+    db.session.add(listen)
+    db.session.commit()
+    return render_template('index.html', count=PageGets.query.count())
 
-@app.route('/about')
-def about():
-    return 'About'
+
+with app.app_context():
+    print("creating tables...")
+    db.create_all()
+    print("created!")
